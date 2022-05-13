3c756e593d19bc94b04e54627bdfd7814991397e
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-05-13 17:47:02 +0200

    Setup the choosing location route

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 
 import "package:flutter/material.dart";
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,14 +12,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String? time = "Loading...";
 
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
     });
   }
 
@@ -33,9 +35,10 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text(time!),
-    ));
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.blue,
+          size: 50.0,
+        )),);
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,34 +1,41 @@
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 
 class WorldTime {
 
-  String? location; // location name of the UI
-  String? time; // the time in that location
-  String? flag; // url to an asset flag icon
-  String? url; // location url for api endpoint
+  String location; // location name of the UI
+  late String time; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+  late bool? isDayTime; // true or false if daytime or not
 
  // create a constructor with the location name and the url
-  WorldTime({this.location, this.flag, this.url});
+  WorldTime({ required this.location, required this.flag, required this.url});
 
 
 
   Future<void> getTime() async{
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
-    //print(data);
+    try {
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    // get properties from data
-    String datetime = data['datetime'];
+      // get properties from data
+      String datetime = data['datetime'];
 
-    // get the offset and convert to duration
-    Duration offset = Duration(hours: int.parse(data['utc_offset'].substring(1,3)));
+      // convert to DateTime and add offset
 
-    DateTime now = (DateTime.parse(datetime)).add(offset);
+      DateTime now = (DateTime.parse(datetime)).add(Duration(hours: int.parse(data['utc_offset'].substring(1,3))));
 
-    // set the time property
-    time = now.toString();
+      // set the time property
+
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    } on Exception catch (e) {
+      print("Error while getting data $e");
+    }
   }
 
 }
